0a9ed48b778128d431aa3878598e03d9864e5026
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-02-28 22:58:21 +0800

    Add nsig decoder

---
+++ src/NSigDecoder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace YouTube;
+
+use YouTube\Exception\YouTubeException;
+
+class NSigDecoder
+{
+    /**
+     * @param string $n_param
+     * @param string $js_code Complete source code for YouTube's player.js
+     * @return string
+     */
+    public function decode(string $n_param, string $js_code): string
+    {
+        $func_name = $this->parseFunctionName($js_code);
+
+        if (!$func_name) {
+            throw new YouTubeException('Failed to extract n function name');
+        }
+
+        $func_code = $this->extractFunctionCode($func_name, $js_code);
+
+        if (!$func_code) {
+            throw new YouTubeException('Failed to extract n function code');
+        }
+
+        return $this->decryptNsig($n_param, $func_name, $func_code);
+    }
+
+    protected function parseFunctionName(string $js_code): ?string
+    {
+        if (preg_match('@(?:\.get\("n"\)\)&&\(b=|(?:b=String\.fromCharCode\(110\)|(?P<str_idx>[a-zA-Z0-9_$.]+)&&\(b="nn"\[\+(?P=str_idx)\])(?:,[a-zA-Z0-9_$]+\(a\))?,c=a\.(?:get\(b\)|[a-zA-Z0-9_$]+\[b\]\|\|null)\)&&\(c=|\b(?P<var>[a-zA-Z0-9_$]+)=)(?P<nfunc>[a-zA-Z0-9_$]+)(?:\[(?P<idx>\d+)\])?\([a-zA-Z]\)(?(var),[a-zA-Z0-9_$]+\.set\((?:"n+"|[a-zA-Z0-9_$]+)\,(?P=var)\))@is', $js_code, $matches)) {
+            $func_name = preg_quote($matches['nfunc']);
+        } else if (preg_match('@;\s*(?P<name>[a-zA-Z0-9_$]+)\s*=\s*function\([a-zA-Z0-9_$]+\)\s*\{(?:(?!};).)+?return\s*(?P<q>["\'])[\w-]+_w8_(?P=q)\s*\+\s*[a-zA-Z0-9_$]+@is', $js_code, $matches)) {
+            $func_name = preg_quote($matches['name']);
+        }
+
+        if (preg_match('@var\s+' . $func_name . '=\[(\S+)\];@is', $js_code, $matches)) {
+            return preg_quote($matches[1]);
+        }
+
+        return null;
+    }
+
+    protected function extractFunctionCode(string $func_name, string $js_code): ?string
+    {
+        if (preg_match('@(function\s+' . $func_name . '\s*\([\s\S]*?})\s+function@is', $js_code, $matches)) {
+            $func_code = $matches[1];
+        } else if (preg_match('@' . $func_name . '\s*=\s*function\s*\(\s*\w+\s*\)\s*{[\s\S]+?};@is', $js_code, $matches)) {
+            $func_code = 'var ' . $matches[0];
+        }
+
+        return preg_replace('@;\s*if\s*\(\s*typeof\s+[a-zA-Z0-9_$]+\s*===?\s*(["\'])undefined\1\s*\)\s*return\s+[\$\w]+;@is', ';', $func_code);
+    }
+
+    protected function decryptNsig(string $n_param, string $func_name, string $func_code): string
+    {
+        $jsrt = new JsRuntime();
+        if ($jsrt->getApp()) {
+            $cache_path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'yt_' . $n_param;
+
+            if (file_put_contents("{$cache_path}.dump", $func_code . "console.log({$func_name}('{$n_param}'));") === false) {
+                throw new YouTubeException('Failed to write file to ' . sys_get_temp_dir());
+
+            } else {
+                exec($jsrt->getApp() . ' ' . $jsrt->getCmd() . " {$cache_path}.dump >{$cache_path}.nsig.tmp");
+                $nsig = trim(file_get_contents("{$cache_path}.nsig.tmp"));
+
+                unlink("{$cache_path}.dump");
+                unlink("{$cache_path}.nsig.tmp");
+            }
+            if (!$nsig || ($nsig == $n_param)) {
+                throw new YouTubeException('Failed to decrypt nsig');
+            }
+        }
+
+        return $nsig ?: $n_param;
+    }
+}
--- src/SignatureLinkParser.php
+++ src/SignatureLinkParser.php
@@ -2,6 +2,7 @@
 
 namespace YouTube;
 
+use YouTube\Exception\YouTubeException;
 use YouTube\Models\StreamFormat;
 use YouTube\Responses\PlayerApiResponse;
 use YouTube\Responses\VideoPlayerJs;
@@ -21,35 +22,57 @@ class SignatureLinkParser
         // final response
         $return = array();
 
+        $decoded_nsig = array();
+
         foreach ($formats_combined as $format) {
 
-            // appear as either "cipher" or "signatureCipher"
-            $cipher = Utils::arrayGet($format, 'cipher', Utils::arrayGet($format, 'signatureCipher', ''));
-
-            // some videos do not need to be decrypted!
             if (isset($format['url'])) {
-                $return[] = new StreamFormat($format);
-                continue;
+                // appear as "url"
+                $url = $format['url'];
+            } else {
+                // appear as either "cipher" or "signatureCipher"
+                $cipher = Utils::arrayGet($format, 'cipher', Utils::arrayGet($format, 'signatureCipher', ''));
+
+                $cipherArray = Utils::parseQueryString($cipher);
+
+                // contains ?ip noting which IP can access it, and ?expire containing link expiration timestamp
+                $url = Utils::arrayGet($cipherArray, 'url');
+                $sp = Utils::arrayGet($cipherArray, 'sp'); // used to be 'sig'
+
+                // needs to be decrypted!
+                $signature = Utils::arrayGet($cipherArray, 's');
             }
-
-            $cipherArray = Utils::parseQueryString($cipher);
-
-            // contains ?ip noting which IP can access it, and ?expire containing link expiration timestamp
-            $url = Utils::arrayGet($cipherArray, 'url');
-            $sp = Utils::arrayGet($cipherArray, 'sp'); // used to be 'sig'
-
-            // needs to be decrypted!
-            $signature = Utils::arrayGet($cipherArray, 's');
 
             $streamUrl = new StreamFormat($format);
 
             if ($playerJs) {
+                if (preg_match('/&n=(.*?)&/', $url, $matches)) {
+                    // decrypt nsig
+                    try {
+                        if ((new JsRuntime())->getApp()) {
+                            $n_param = $matches[1];
 
-                $decoded_signature = (new SignatureDecoder())->decode($signature, $playerJs->getResponseBody());
-                $decoded_url = $url . '&' . $sp . '=' . $decoded_signature;
+                            if (!array_key_exists($n_param, $decoded_nsig)) {
+                                $decoded_nsig[$n_param] = (new NSigDecoder())->decode($n_param, $playerJs->getResponseBody());
+                            }
+                            if ($decoded_nsig[$n_param] != $n_param) {
+                                $url = str_replace('&n=' . $n_param . '&', '&n=' . $decoded_nsig[$n_param] . '&', $url);
+                            }
+                        }
+                    } catch (YouTubeException $e) {
+                        $streamUrl->_error = 'Unable to decrypt nsig: ' . $e->getMessage() . '. This URL may yield HTTP Error 403.';
+                    }
+                }
 
-                $streamUrl->url = $decoded_url;
+                if (isset($format['url'])) {
+                    // some videos do not need signature decryption
+                    $streamUrl->url = $url;
+                } else {
+                    $decoded_signature = (new SignatureDecoder())->decode($signature, $playerJs->getResponseBody());
+                    $decoded_url = $url . '&' . $sp . '=' . urlencode($decoded_signature);
 
+                    $streamUrl->url = $decoded_url;
+                }
             } else {
                 $streamUrl->url = $url;
             }
