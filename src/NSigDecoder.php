667c1e5aedb0a3d34b2246a9cccff35eb7177316
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-04-24 03:28:58 +0800

    fix nsig extraction

--- src/NSigDecoder.php
+++ src/NSigDecoder.php
@@ -30,13 +30,15 @@ class NSigDecoder
 
     protected function parseFunctionName(string $js_code): ?string
     {
-        if (preg_match('@(?:\.get\("n"\)\)&&\(b=|(?:b=String\.fromCharCode\(110\)|(?P<str_idx>[a-zA-Z0-9_$.]+)&&\(b="nn"\[\+(?P=str_idx)\])(?:,[a-zA-Z0-9_$]+\(a\))?,c=a\.(?:get\(b\)|[a-zA-Z0-9_$]+\[b\]\|\|null)\)&&\(c=|\b(?P<var>[a-zA-Z0-9_$]+)=)(?P<nfunc>[a-zA-Z0-9_$]+)(?:\[(?P<idx>\d+)\])?\([a-zA-Z]\)(?(var),[a-zA-Z0-9_$]+\.set\((?:"n+"|[a-zA-Z0-9_$]+)\,(?P=var)\))@is', $js_code, $matches)) {
+        if (preg_match('@[;\n](?:(?P<f>function\s+)|(?:var\s+)?)(?P<funcname>[a-zA-Z0-9_$]+)\s*(?(f)|=\s*function\s*)\((?P<argname>[a-zA-Z0-9_$]+)\)\s*\{(?:(?!\}[;\n]).)+\}\s*catch\(\s*[a-zA-Z0-9_$]+\s*\)\s*\{\s*return\s+[\w\$]+\[\d+]\s*\+\s*(?P=argname)\s*\}\s*return\s+[^}]+\}[;\n]@xs', $js_code, $matches)) {
+            return preg_quote($matches['funcname']);
+        } else if (preg_match('@(?:\.get\("n"\)\)&&\(b=|(?:b=String\.fromCharCode\(110\)|(?P<str_idx>[a-zA-Z0-9_$.]+)&&\(b="nn"\[\+(?P=str_idx)\])(?:,[a-zA-Z0-9_$]+\(a\))?,c=a\.(?:get\(b\)|[a-zA-Z0-9_$]+\[b\]\|\|null)\)&&\(c=|\b(?P<var>[a-zA-Z0-9_$]+)=)(?P<nfunc>[a-zA-Z0-9_$]+)(?:\[(?P<idx>\d+)\])?\([a-zA-Z]\)(?(var),[a-zA-Z0-9_$]+\.set\((?:"n+"|[a-zA-Z0-9_$]+)\,(?P=var)\))@x', $js_code, $matches)) {
             $func_name = preg_quote($matches['nfunc']);
-        } else if (preg_match('@;\s*(?P<name>[a-zA-Z0-9_$]+)\s*=\s*function\([a-zA-Z0-9_$]+\)\s*\{(?:(?!};).)+?return\s*(?P<q>["\'])[\w-]+_w8_(?P=q)\s*\+\s*[a-zA-Z0-9_$]+@is', $js_code, $matches)) {
+        } else if (preg_match('@;\s*(?P<name>[a-zA-Z0-9_$]+)\s*=\s*function\([a-zA-Z0-9_$]+\)\s*\{(?:(?!};).)+?return\s*(?P<q>["\'])[\w-]+_w8_(?P=q)\s*\+\s*[a-zA-Z0-9_$]+@xs', $js_code, $matches)) {
             $func_name = preg_quote($matches['name']);
         }
 
-        if (preg_match('@var\s+' . $func_name . '=\[(\S+)\];@', $js_code, $matches)) {
+        if (preg_match('@var\s+' . $func_name .  '\s*=\s*\[(.+?)\]\s*[,;]@', $js_code, $matches)) {
             return preg_quote($matches[1]);
         }
 
@@ -46,12 +48,12 @@ class NSigDecoder
     protected function extractFunctionCode(string $func_name, string $js_code): ?string
     {
         $var_code = '';
-        if (preg_match('@;\s*(?P<code>var\s+[\w$]+\s*=\s*(?P<q2>["\'])[\s\S]+?(?P=q2)\.split\((?P<q3>["\'])[\s\S]+?(?P=q3)\)|\[\s*(?:(?P<q4>["\'])[\s\S]*(?P=q4)\s*,?\s*)+\])[;,]@i', $js_code, $matches)) {
+        if (preg_match('@(?P<q1>["\'])use\s+strict(?P=q1);\s*(?P<code>var\s+(?P<name>[\w$]+)\s*=\s*(?P<value>(?P<q2>["\'])(?:(?!(?P=q2)).|\\.)+(?P=q2)\.split\((?P<q3>["\'])(?:(?!(?P=q3)).)+(?P=q3)\)|\[\s*(?:(?P<q4>["\'])(?:(?!(?P=q4)).|\\.)*(?P=q4)\s*,?\s*)+\]))[;,]@x', $js_code, $matches)) {
             $var_code = $matches['code'] . ";\n";
         }
 
-        if (preg_match('@' . $func_name . '\s*=\s*function\s*\(\s*\w+\s*\)\s*{[\s\S]+?};@', $js_code, $matches)) {
-            $func_code = 'var ' . $matches[0];
+        if (preg_match("@[{;,]\s*((?:function\s+{$func_name}|{$func_name}\s*=\s*function|(?:var|const|let)\s+{$func_name}\s*=\s*function)\s*\([^\)]*\)\s*{.+?};)@xs", $js_code, $matches)) {
+            $func_code = (strpos($matches[1], $func_name) === 0 ? 'var ' : '') . $matches[1];
         } else if (preg_match('@(function\s+' . $func_name . '\s*\([\s\S]*?})\s+function@', $js_code, $matches)) {
             $func_code = $matches[1];
         }
