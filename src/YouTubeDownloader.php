450736d8432dd288ffcb181d9b30c6fdf0e1d479
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-09-26 21:35:14 +0800

    force player '0004de42'

--- src/YouTubeDownloader.php
+++ src/YouTubeDownloader.php
@@ -160,7 +160,8 @@ class YouTubeDownloader
                 'playbackContext' => [
                     'contentPlaybackContext' => [
                         'html5Preference' => 'HTML5_PREF_WANTS',
-                        'signatureTimestamp' => (int)$sig_timestamp,
+                        // force player '0004de42' and signature timestamp 20348
+                        'signatureTimestamp' => 20348,  //(int)$sig_timestamp,
                     ]
                 ],
                 'racyCheckOk' => true,
@@ -245,7 +246,10 @@ class YouTubeDownloader
             }
 
             // get player.js location that holds URL signature decipher function
-            $player_url = $page->getPlayerScriptUrl();
+            //$player_url = $page->getPlayerScriptUrl();
+
+            // force player '0004de42' and signature timestamp 20348
+            $player_url = 'https://www.youtube.com/s/player/0004de42/player_ias.vflset/en_US/base.js';
             $response = $this->client->get($player_url);
             $player = new VideoPlayerJs($response);
 
