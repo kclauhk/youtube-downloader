c35f2d35cd3f9f1cd871f6280de8dd7d5a67dfc0
kclauhk <78251477+kclauhk@users.noreply.github.com>
2025-11-18 03:28:45 +0800

    add JS challenge solver

--- src/SignatureLinkParser.php
+++ src/SignatureLinkParser.php
@@ -22,12 +22,21 @@ class SignatureLinkParser
         // final response
         $return = array();
 
-        $s_decoder = new SignatureDecoder();
         $n_decoder = new NSigDecoder();
-        $decoded_nsig = array();
+        $s_decoder = new SignatureDecoder();
+        $decoded_n = array();
+        $decoded_s = array();
+        $ciphers = array();
+        $n_params = array();
+        $signatures = array();
 
-        foreach ($formats_combined as $format) {
+        if (preg_match($n_decoder::REGEX_RETURN_CODE, $playerJs->getResponseBody())) {
+            $use_solver = false;
+        } else {
+            $use_solver = true;
+        }
 
+        foreach ($formats_combined as $k=>$format) {
             if (isset($format['url'])) {
                 // appear as "url"
                 $url = $format['url'];
@@ -42,23 +51,34 @@ class SignatureLinkParser
                 $sp = Utils::arrayGet($cipherArray, 'sp'); // used to be 'sig'
 
                 // needs to be decrypted!
-                $signature = Utils::arrayGet($cipherArray, 's');
+                if ($signature = Utils::arrayGet($cipherArray, 's')) {
+                    $signatures[] = $signature;
+                    $ciphers[$k] = [$signature, $sp, $url];
+                }
             }
 
+            if ($use_solver) {
+                if (preg_match('/&n=(.*?)&/', ($url ?? ''), $matches)) {
+                    $n_params[] = $matches[1];
+                }
+                continue;
+            }
+
+            // don't use JsChallengeSolver (deprecated but kept as fallback)
             $streamUrl = new StreamFormat($format);
 
             if ($playerJs) {
                 if (preg_match('/&n=(.*?)&/', ($url ?? ''), $matches)) {
-                    // decrypt nsig
+                    // decrypt n
                     try {
                         if ((new JsRuntime())->getApp()) {
                             $n_param = $matches[1];
 
-                            if (!array_key_exists($n_param, $decoded_nsig)) {
-                                $decoded_nsig[$n_param] = $n_decoder->decode($n_param, $playerJs->getResponseBody());
+                            if (!array_key_exists($n_param, $decoded_n)) {
+                                $decoded_n[$n_param] = $n_decoder->decode($n_param, $playerJs->getResponseBody());
                             }
-                            if ($decoded_nsig[$n_param] != $n_param) {
-                                $url = str_replace('&n=' . $n_param . '&', '&n=' . $decoded_nsig[$n_param] . '&', $url);
+                            if ($decoded_n[$n_param] != $n_param) {
+                                $url = str_replace('&n=' . $n_param . '&', '&n=' . $decoded_n[$n_param] . '&', $url);
                             }
                         }
                     } catch (YouTubeException $e) {
@@ -84,6 +104,54 @@ class SignatureLinkParser
             }
 
             $return[] = self::detectSR($streamUrl);
+        }
+
+        if ($use_solver) {
+            $error = null;
+
+            if (($n_params || $signatures) && $playerJs) {
+                $n_params = array_unique($n_params);
+                $signatures = array_unique($signatures);
+
+                try {
+                    if ((new JsRuntime())->getApp()) {
+                        $solver = new JsChallengeSolver();
+                        if ($result = $solver->solve($n_params, $signatures, $playerJs->getResponseBody())) {
+                            $decoded_n = $result[0]['data'];
+                            $decoded_s = $result[1]['data'];
+                        }
+                    }
+                } catch (YouTubeException $e) {
+                    $error = "Unable to solve JS challenges: {$e->getMessage()}. This URL may yield HTTP 403 Forbidden error. (player: {$playerJs->getResponse()->info->url})";
+                }
+            }
+
+            foreach ($formats_combined as $k=>$format) {
+                $streamUrl = new StreamFormat($format);
+
+                if (array_key_exists($k, $ciphers)) {
+                    if (array_key_exists($ciphers[$k][0], $decoded_s)) {
+                        $streamUrl->url = "{$ciphers[$k][2]}&{$ciphers[$k][1]}=" . urlencode($decoded_s[$ciphers[$k][0]]);
+                    } else {
+                        $streamUrl->url = $ciphers[$k][2];
+                        if ($error) {
+                            $streamUrl->_error[] = $error;
+                        }
+                    }
+                } elseif (empty($format['url'])) {
+                    continue;
+                }
+
+                if (preg_match('/&n=(.*?)&/', ($streamUrl->url ?? ''), $matches)) {
+                    if (array_key_exists($matches[1], $decoded_n)) {
+                        $streamUrl->url = str_replace("&n={$matches[1]}&", "&n={$decoded_n[$matches[1]]}&", $streamUrl->url);
+                    } elseif ($error) {
+                        $streamUrl->_error[] = $error;
+                    }
+                }
+
+                $return[] = self::detectSR($streamUrl);
+            }
         }
 
         return $return;
